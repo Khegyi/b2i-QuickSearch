02263023c312ddc0e2fa8af16e381f7430d4a44b
Khegyi <14124275+Khegyi@users.noreply.github.com.>
2024-06-25 21:45:54 +0200

    added new selected concept display component

---
+++ quick-search/app/components/DisplayConcept.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface DisplayConceptProps {
+  selectedConcept: { id: string; term: string } | null;
+}
+
+const DisplayConcept: React.FC<DisplayConceptProps> = ({ selectedConcept }) => {
+  return (
+    <div className="text-md ">
+      <span>Selected Concept:</span>
+      <div>
+        {selectedConcept !== null
+          ? selectedConcept?.id + " | " + selectedConcept?.term
+          : "None"}{" "}
+      </div>
+    </div>
+  );
+};
+
+export default DisplayConcept;
